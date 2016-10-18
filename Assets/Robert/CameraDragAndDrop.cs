10a0e02dd8a6d8cb5e82ae063fea39d0796cbcfa
Robert Schendzielorz <r.t.s@web.de>
2016-10-18 14:56:57 +0200

    Hacker Scene update

--- Assets/Robert/CameraDragAndDrop.cs
+++ Assets/Robert/CameraDragAndDrop.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class CameraDragAndDrop : MonoBehaviour {
 
+	[SerializeField]
+	int mouseRegion;
+
 	bool _dragging = false;
 	Camera _camera;
 
@@ -17,7 +20,7 @@ public class CameraDragAndDrop : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown(0))
+		if (Input.GetMouseButtonDown(0) && MouseIsInScreenRegion(mouseRegion))
 		{
 			StartDragging();
 			_startPos = Input.mousePosition;
@@ -36,6 +39,48 @@ public class CameraDragAndDrop : MonoBehaviour {
 			_newPos = _startPos - Input.mousePosition;
 			this.transform.position = new Vector3(_startTransformPos.x + _newPos.x*scrollPos/50, 1, _startTransformPos.z + _newPos.y*scrollPos/50);
 		}
+	}
+	/// <summary>
+	/// Returns true if mouse is in given part of the screen. 
+	/// Each part is a quarter of the screen in width and height starting from bottom left, then bottom right, then top left and top right.
+	/// 3|4
+	/// -|-
+	/// 1|2
+	/// 
+	/// </summary>
+	/// <param name="regionNumber"></param>
+	/// <returns></returns>
+	bool MouseIsInScreenRegion(int regionNumber)
+	{
+		if(Input.mousePosition.x < Screen.width/2 && Input.mousePosition.x > 0 && Input.mousePosition.y < Screen.height/2 && Input.mousePosition.y > 0)
+		{
+			if(regionNumber == 1)
+			{
+				return true;
+			}
+		}
+		if (Input.mousePosition.x < Screen.width && Input.mousePosition.x > Screen.width/2 && Input.mousePosition.y < Screen.height / 2 && Input.mousePosition.y > 0)
+		{
+			if (regionNumber == 2)
+			{
+				return true;
+			}
+		}
+		if (Input.mousePosition.x < Screen.width / 2 && Input.mousePosition.x > 0 && Input.mousePosition.y < Screen.height && Input.mousePosition.y > Screen.height/2)
+		{
+			if (regionNumber == 3)
+			{
+				return true;
+			}
+		}
+		if (Input.mousePosition.x < Screen.width && Input.mousePosition.x > Screen.height/2 && Input.mousePosition.y < Screen.height && Input.mousePosition.y > Screen.height/2)
+		{
+			if (regionNumber == 4)
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 	void StartDragging()
