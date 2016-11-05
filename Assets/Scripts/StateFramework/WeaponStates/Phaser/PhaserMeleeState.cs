58dadffdce3ceeb60e4057d2c2d1d8451a52532b
Daniel Henseler <danielhenseler@badcodes.de>
2016-11-05 15:13:43 +0100

    Added States for guns

---
+++ Assets/Scripts/StateFramework/WeaponStates/Phaser/PhaserMeleeState.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace StateFramework {
+	public class PhaserMeleeState : AbstractWeaponState {
+		protected StateMachine<AbstractWeaponState> _fsm = null;
+		protected Weapon _weapon;
+
+		public PhaserMeleeState(Weapon pWeapon, StateMachine<AbstractWeaponState> pFsm) : base(pWeapon, pFsm) {
+			_weapon = pWeapon;
+			_fsm = pFsm;
+		}
+
+		public override void Enter() { }
+		public override void Step() { }
+		public override void Exit() { }
+	}
+}
---
+++ Assets/Scripts/StateFramework/WeaponStates/Phaser/PhaserReloadState.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace StateFramework {
+	public class PhaserReloadState : AbstractWeaponState {
+		protected StateMachine<AbstractWeaponState> _fsm = null;
+		protected Weapon _weapon;
+
+		public PhaserReloadState(Weapon pWeapon, StateMachine<AbstractWeaponState> pFsm) : base(pWeapon, pFsm) {
+			_weapon = pWeapon;
+			_fsm = pFsm;
+		}
+
+		public override void Enter() { }
+		public override void Step() { }
+		public override void Exit() { }
+	}
+}
