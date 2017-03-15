2217899249db8563c6d0ff0dc60261f4d266ae4f
Daniel Henseler <danielhenseler@badcodes.de>
2017-03-15 17:49:34 +0100

    Changed Reference Manager

--- Assets/Scripts/Hacker/HackerReferenceManager.cs
+++ Assets/Scripts/Hacker/HackerReferenceManager.cs
@@ -3,43 +3,21 @@ using System.Collections;
 using Gamelogic.Extensions;
 
 public class HackerReferenceManager : Singleton<HackerReferenceManager> {
-	[Header("Minimap Icon Sprites")]
-	public Sprite KeycardIcon;
-	public Sprite HealthpackIcon;
-	public Sprite AmmobagIcon;
-
     [Header("Minimap Icon Prefabs")]
     public GameObject PlayerIcon;
     public GameObject DoorIcon;
-    public GameObject FireWallIcon;
-    public GameObject PickupIcon;
 	public GameObject DroneIcon;
     public GameObject CameraIcon;
     public GameObject TurretIcon;
+	public GameObject GasPipeIcon;
+	public GameObject FuseBoxIcon;
+	public GameObject DefenceWallIcon;
+	public GameObject LightIcon;
+	public GameObject SpeakerIcon;
+	public GameObject VendingMachineIcon;
+	public GameObject DatabaseIcon;
+	public GameObject SecurityStationIcon;
 
 	[Header("Minimap Prefabs")]
 	public GameObject LaserShot;
-
-	[Header("Network Avatar Prefabs")]
-	public GameObject HackerAvatar;
-	public GameObject AdminAvatar;
-
-	[Header("Network Packet Prefabs")]
-	public GameObject AlarmPacket;
-	public GameObject FeedbackPacket;
-	public GameObject SuspicionPacket;
-
-	[Header("Network Node Prefabs")]
-	public GameObject BaseNode;
-	public GameObject DatabaseNode;
-	public GameObject DoorNode;
-	public GameObject FirewallNode;
-	public GameObject DispenserNode;
-	public GameObject HackerNode;
-	public GameObject SecurityNode;
-	public GameObject CameraNode;
-	public GameObject TurretNode;
-
-	[Header("Other Network Prefabs")]
-	public GameObject ConnenctionLine;
 }
