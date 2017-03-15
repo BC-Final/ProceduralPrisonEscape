6c53e6bcae8fb98488170c48e2f718907f9efa97
Robert Schendzielorz <Robert Schendzielorz>
2017-03-15 20:16:13 +0100

    Meta und scene change

---
+++ Assets/Scripts/Hacker/Data/PlayerMapIcon.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMapIcon : AbstractMapIcon
+{
+    public static void CreateInstance(Vector2 pPos, float rot)
+    {
+        GameObject go = (GameObject)Instantiate(HackerReferenceManager.Instance.DoorIcon, new Vector3(pPos.x / MinimapManager.scale, pPos.y / MinimapManager.scale, 0), Quaternion.Euler(0, rot, 0));
+    }
+
+    private Vector3 _oldPos;
+	private Vector3 _newPos;
+
+	private Quaternion _oldRot;
+	private Quaternion _newRot;
+
+	private float _lastUpdateTime = 0.0f;
+
+	private float _currentLerpTime = 0.0f;
+	private float _lerpTime = 0.5f;
+
+	// Update is called once per frame
+	private void Update () {
+		_currentLerpTime += Time.deltaTime;
+		if (_currentLerpTime > _lerpTime) {
+			_currentLerpTime = _lerpTime;
+		}
+
+		if (_lerpTime == 0.0f) {
+			_lerpTime = 0.5f;
+		}
+
+		float perc = _currentLerpTime / _lerpTime;
+
+		transform.position = Vector3.Lerp(_oldPos, _newPos, perc);
+		transform.rotation = Quaternion.Slerp(_oldRot, _newRot, perc);
+	}
+
+	public void InitialTransform (Vector3 pPos, float pRot) {
+		_lastUpdateTime = Time.time;
+
+		_oldPos = pPos;
+		_newPos = pPos;
+
+		_oldRot = Quaternion.Euler(0, pRot, 0);
+		_newRot = Quaternion.Euler(0, pRot, 0);
+	}
+
+	public void UpdateTransform (Vector3 pPos, float pRot) {
+		_lerpTime = Time.time - _lastUpdateTime;
+		_lastUpdateTime = Time.time;
+		_currentLerpTime = 0f;
+
+		_oldPos = transform.position;
+		_newPos = pPos;
+
+		_oldRot = transform.rotation;
+		_newRot = Quaternion.Euler(0, pRot, 0);
+	}
+}
