c794d97dd85a7cf40c8365457f0006c067ffc1fe
Daniel Henseler <danielhenseler@badcodes.de>
2017-05-06 23:32:52 +0200

    Compass upgrade

--- Assets/CompassShooterUI.cs
+++ Assets/CompassShooterUI.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CompassShooterUI : MonoBehaviour {
 	[SerializeField]
 	private float _width = 400.0f;
+
+	[SerializeField]
+	private float _height = 100.0f;
 
 	[SerializeField]
 	private RectTransform _north;
@@ -20,6 +24,10 @@ public class CompassShooterUI : MonoBehaviour {
 
 	private Transform _player;
 
+	private Color _black = new Color(0.0f, 0.0f, 0.0f, 1.0f);
+	private Color _grey = new Color(0.25f, 0.25f, 0.25f, 1.0f);
+	private Color _greyFaded = new Color(0.25f, 0.25f, 0.25f, 0.50f);
+
 	private void Start() {
 		_player = FindObjectOfType<PlayerHealth>().transform;
 	}
@@ -33,26 +41,26 @@ public class CompassShooterUI : MonoBehaviour {
 		//(1, 0) = NORTH
 		//(-1, 0) = SOUTH
 
-		Debug.Log("X: " + xDot + "; Y: " + yDot);
-
 		//if xDot > 0 dont show S
 		//if xDot < 0 dont show N
 		//if yDot > 0 dont show W
 		//if yDot < 0 dont show E
 
-		_south.gameObject.SetActive(xDot <= 0.0f);
-		_north.gameObject.SetActive(xDot >= 0.0f);
-		_west.gameObject.SetActive(yDot <= 0.0f);
-		_east.gameObject.SetActive(yDot >= 0.0f);
+		//_south.GetComponent<Text>().color = (xDot <= 0.0f) ? Color.black : Color.grey;
+		//_north.GetComponent<Text>().color = (xDot >= 0.0f) ? Color.black : Color.grey;
+		//_west.GetComponent<Text>().color = (yDot <= 0.0f) ? Color.black : Color.grey;
+		//_east.GetComponent<Text>().color = (yDot >= 0.0f) ? Color.black : Color.grey;
 
-		_south.anchoredPosition = new Vector2(Utilities.Math.Remap(Mathf.Abs(xDot), 0, 1, (yDot > 0) ? _width / 2.0f : -_width/2.0f, (yDot > 0) ? 0.0f : 0.0f), 0);
-		_north.anchoredPosition = new Vector2(Utilities.Math.Remap(Mathf.Abs(xDot), 0, 1, (yDot < 0) ? _width / 2.0f : -_width / 2.0f, (yDot < 0) ? 0.0f : 0.0f), 0);
-		_west.anchoredPosition = new Vector2(Utilities.Math.Remap(Mathf.Abs(yDot), 0, 1, (xDot < 0) ? _width / 2.0f : -_width / 2.0f, (xDot < 0) ? 0.0f : 0.0f), 0);
-		_east.anchoredPosition = new Vector2(Utilities.Math.Remap(Mathf.Abs(yDot), 0, 1, (xDot > 0) ? _width / 2.0f : -_width / 2.0f, (xDot > 0) ? 0.0f : 0.0f), 0);
+		//_south.GetComponent<Text>().color = Color.Lerp((xDot < 0) ? _grey : _greyFaded, (xDot < 0) ? _black : _grey, Mathf.Abs(yDot));
+		_south.GetComponent<Text>().color = Color.Lerp((xDot < 0) ? _black : _greyFaded, _grey, Mathf.Abs(yDot));
+		_north.GetComponent<Text>().color = Color.Lerp((xDot > 0) ? _black : _greyFaded, _grey, Mathf.Abs(yDot));
 
-		//Vector3 perp = Vector3.Cross(Vector3.forward, _player.forward);
-		//float dir = Vector3.Dot(perp, Vector3.up);
+		_west.GetComponent<Text>().color = Color.Lerp((yDot < 0) ? _black : _greyFaded, _grey, Mathf.Abs(xDot));
+		_east.GetComponent<Text>().color = Color.Lerp((yDot > 0) ? _black : _greyFaded, _grey, Mathf.Abs(xDot));
 
-
+		_south.anchoredPosition = new Vector2(yDot * _width / 2.0f, -xDot * _height / 2.0f);
+		_north.anchoredPosition = new Vector3(-yDot * _width / 2.0f, xDot * _height / 2.0f);
+		_east.anchoredPosition = new Vector2(xDot * _width / 2.0f, yDot * _height / 2.0f);
+		_west.anchoredPosition = new Vector3(-xDot * _width / 2.0f, -yDot * _height / 2.0f);
 	}
 }
